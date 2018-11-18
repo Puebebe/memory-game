46523988f6ee409becf510340a6ad5dcdb915b75
Patrick Parafińczuk <44815944+patrykparafinczuk@users.noreply.github.com>
2018-11-18 21:05:32 +0100

    Update script.js
    Added comparizer function that is not completed yet and added array named array_holder.

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,17 @@
+var array_holder = [];
 function startGame() {
+	var pools = document.getElementsByClassName("image-pool");
 	var array_png = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 	var images_sources = document.getElementsByClassName("images");
 	var array_picker;
 	var array_index;
 	for(var i=0;i<16;i++) {
 		do {
+			images_sources[i].style.visibility = "hidden";
 			array_picker = Math.floor(Math.random()*16);
 			array_index = array_png[array_picker];
 			if (array_index!="none") {
-				images_sources[i].src = "images/"+array_index+".png";
+				array_holder[i] = "images/"+array_index+".png";
 				array_png[array_picker] = "none";
 				array_index = "none";
 			}
@@ -19,5 +22,12 @@ function startGame() {
 		}
 		while(array_index != "none");
 		var array_index;
+		pools[i].style.visibility = "visible";
 	}
+}
+function comparizer(n) {
+	var images_sources = document.getElementsByClassName("images");
+	images_sources[n].src = array_holder[n];
+	images_sources[n].style.visibility = "visible";
+	
 }
